c86f2438e9d94449e6e706f19c209b02b9e75df5
Dikshit <dikshitkathuria1803@gmail.com>
2019-04-22 12:34:31 +0530

    Intial Commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const hbs = require('hbs')
+// const filePath = path.join(__dirname , '../')
+
+//Define Paths for express config
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+
+//Setup handlebars engine and views location
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+app.get('',(req,res)=>{
+    res.render('index',{
+        name:'dikshit',
+        age:23
+    })
+})
+app.listen(5000,()=>{
+    console.log('App is running');
+})
